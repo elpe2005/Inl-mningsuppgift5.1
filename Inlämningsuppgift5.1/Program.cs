01cb60649bd2e2e24c3fbeff9a24138049730381
Bob156590 <gujo04002@utb.vaxjo.se>
2023-01-20 10:13:00 +0100

    Add project files.

---
+++ Inlämningsuppgift5.1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace uppgift51
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] meningar = { "Hej jag heter Elliot", "Jag bor i Växjö", "Jag går på teknikum" };
+
+            for (int i = 0; i < meningar.Length; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine(meningar[i]);
+            }
+        }
+    }
+}
